08b03352bc062f9d17db56622277d3162930b7c7
alejandro <aleinsti94@gmail.com>
2025-03-12 12:53:19 -0400

    first commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL; // Importa el facade URL
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,8 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if ($this->app->environment('production')) {
-            URL::forceScheme('https');
+            URL::forceScheme('https'); // Fuerza HTTPS en producción
         }
-       
     }
 }
