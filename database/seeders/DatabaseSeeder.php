4a9eaa0fcef1eaec4e4dd42e4e69ba7a559f1939
alejandro <aleinsti94@gmail.com>
2025-03-12 13:54:32 -0400

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,17 +3,17 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run()
+    public function run(): void
     {
-        DB::table('users')->insert([
-            'name' => 'Admin User',
+       
+
+        \App\Models\User::factory()->create([
+        //     'name' => 'Admin User',
             'email' => 'admin@example.com',
             'password' => Hash::make('admin123'), // La contraseña será 'admin123'
             'role' => 'admin',
